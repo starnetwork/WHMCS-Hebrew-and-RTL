21c086e3841f4bd8359bd38686b7888bbe207bf2
Shalom Burla <shalom@starltd.net>
2014-01-09 16:46:45 +0200

    test

---
+++ addons/PasswordManager/lang/english.php
@@ -0,0 +1,35 @@
+<?php
+$LANG['validation_required']= 'Field % is required';
+$LANG['validation_date']    = 'Incorrect date in %';
+$LANG['validation_datetime']= 'Incorrect date in %';
+$LANG['validation_is_int']  = 'Field % must be integer value';
+$LANG['password_manager']   = 'Password Manager';
+$LANG['name']               = 'Name';
+$LANG['username']           = 'Username';
+$LANG['password']           = 'Password';
+$LANG['notes']              = 'Note';
+$LANG['actions']            = 'Actions';
+$LANG['add_password']       = 'Add New Password';
+$LANG['copy_to_clipboard']  = 'Show Password';
+$LANG['login_page']         = 'Login&nbsp;Page';
+$LANG['edit']               = 'Edit';
+$LANG['remove']             = 'Remove';
+$LANG['products_services']  = 'Products & Services Passwords';
+$LANG['billingcycle']       = 'Billing Cycle';
+$LANG['nextrenew']          = 'Next Renew';
+$LANG['ips']                = 'IP Address';
+$LANG['details']            = 'Details';
+$LANG['password_edit']      = 'Passwords Manager - Edit Password';
+$LANG['website']            = 'Website';
+$LANG['edit_to_change']     = 'edit to change password';
+$LANG['loginurl']           = 'Login URL';
+$LANG['visibility']         = 'Visibility For Admins';
+$LANG['checktoallow']       = 'Check to allow';
+$LANG['add_new_password']   = 'Password Manager - Add New Password';
+$LANG['back_to']            = 'Back to Password Manager';
+$LANG['no_permission']      = 'Error: There is no Permissions to this sub-account';
+$LANG['confirm_delete']     = 'Are you sure to make this action?';
+$LANG['edit_password']      = 'Save Login Details';
+$LANG['hide']               = 'Hide';
+$LANG['url_is_empty']       = 'URL is empty.';
+
