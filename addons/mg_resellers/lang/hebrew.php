2b01d7d789c76d8da6ad4f5028584a7007b2bd3b
Shalom Burla <shalom@starltd.net>
2014-04-16 13:52:59 +0300

    ModulesGaredn Resellers module Lang Files

---
+++ addons/mg_resellers/lang/hebrew.php
@@ -0,0 +1,67 @@
+<?php
+
+$LANG['res_back']					= "חזרה";
+$LANG['res_enable_as_subclient']    = "Enable as Subclient";
+$LANG['res_vat_id']                 = "VAT ID";
+$LANG['res_send_contacts_invoices'] = "Send contact's invoices";
+$LANG['res_products_services']      = "Products/Services";
+$LANG['res_disabled']               = "(disabled)";
+$LANG['res_domains']                = "Domains";
+$LANG['res_management_area']        = "מרכז המפיצים - ניהול תתי לקוחות ומיתוג";
+$LANG['res_provide_link']           = "Provide that link to your customers";
+$LANG['res_advanced_links']         = "Advanced Links";
+$LANG['res_note']                   = "If you would like to integrate your website with reseller account, you could use those links:";
+$LANG['res_update_ticket']          = "Update Ticket";
+$LANG['res_your_domain']			= "Your Domain";
+$LANG['res_your_domain_help']		= 'If you would like to use your own domain instead of prepared link, then pass it there and create <a href="http://en.wikipedia.org/wiki/CNAME_record" target="_blank">CNAME</a> record in DNS settings. Contact administrator for more details.';
+$LANG['res_companyname']			= 'Company Name';
+$LANG['res_companyname_help']		= 'Company name will be used for email templates generation';
+$LANG['res_templates_help']			= 'Click on template in order to edit it for your subclients';
+$LANG['res_subclients_assigned']	= 'Subclient assigned:';
+$LANG['res_subclient']				= 'Subclient';
+$LANG['res_new_contact']			= 'New Subclient';
+
+$LANG['res_field']					= "Field";
+$LANG['res_value']					= "Value";
+$LANG['res_save']					= "Save";
+$LANG['res_custom_logo']            = "Custom Logo";
+$LANG['res_custom_logo_help']       = "You can paste logo URL or upload file from hard drive.";
+$LANG['res_custom_logo_not_found']  = "- none -";
+$LANG['res_custom_logo_preview']    = "[preview]";
+$LANG['res_custom_logo_edit']       = "[edit]";
+$LANG['res_custom_logo_failed']     = "Image cannot be loaded!";
+$LANG['res_info_domain_updated']        = "Domain has been updated";
+$LANG['res_error_domain_not_updated']   = "Domain has not been updated";
+$LANG['res_error_domain_invalid']       = "Domain is not valid";
+$LANG['res_info_logo_uploaded']         = "Logo File Uploaded";
+$LANG['res_error_logo_not_uploaded']    = "Error uploading logo file. Is it image?";
+// templates
+$LANG['res_templates']				= "Email Templates";
+$LANG['res_no_available_templates']	= "No Available Email Templates";
+
+$LANG['res_subject']				= "Subject";
+$LANG['res_fromname']				= "From";
+// @deprecated $LANG['res_fromemail']				= "From Email";
+$LANG['res_message']				= "Message";
+$LANG['res_save']					= "Save";
+$LANG['res_load_from_default']		= "Load From Default Template";
+$LANG['res_load_alert']				= "Are you sure to reset to default template?";
+$LANG['res_template_not_saved']		= "Those values are auto-generated - not saved";
+
+// form errors
+$LANG['res_form_subject_not_empty']	= "Field 'Subject' is required and cannot be empty";
+$LANG['res_form_message_not_empty']	= "Field 'Message' is required and cannot be empty";
+$LANG['res_form_fromname_not_empty']= "Field 'From' is required and cannot be empty";
+// @deprecated $LANG['res_form_fromemail_not_empty'] = "Field 'From Email' is required and cannot be empty";
+$LANG['res_form_subject_too_long']	= "Content of field 'Subject' is too short or too long";
+$LANG['res_form_message_too_long']	= "Content of field 'Message' is too short or too long";
+$LANG['res_form_fromname_too_long']	= "Content of field 'From' is too short or too long";
+// @deprecated $LANG['res_form_fromemail_too_long']= "Content of field 'From Email' is too short or too long";
+
+//$LANG['res_validate_firstname']		= 'Field "firstname" is not valid';
+//$LANG['res_validate_lastname']		= 'Field "lastname" is not valid';
+//$LANG['res_validate_email']			= 'Field "email" is not valid';
+//$LANG['res_validate_address1']		= 'Field "address 1" is not valid';
+//$LANG['res_validate_city']			= 'Field "city" is not valid';
+//$LANG['res_validate_pass']			= 'Field "password" is not valid';
+//$LANG['res_validate_captcha']		= 'Field "captcha" is not valid';
