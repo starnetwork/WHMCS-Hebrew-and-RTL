299ba4da5588267d73a171c179afb1908526451d
Shalom Burla <shalom@starltd.net>
2014-04-16 11:29:38 +0300

    DNS Manager Translation

---
+++ templates/dns_templates/reverse_dns/lang/hebrew.php
@@ -0,0 +1,41 @@
+<?php
+
+$lang = array(
+    'main_header' => 'ניהול DNS הופכי',
+    'add_ptr_add_button' => 'הוסף',
+    'add_ptr_cancel_button' => 'ביטול',
+    'add_ptr_lbl_zone_name' => 'שם אזור:',
+    'add_ptr_lbl_zone_ip' => 'IP:',
+    'add_ptr_lbl_no_ip' => 'אין IPs',
+    'add_ptr_lbl_ptr_name' => 'שם PTR:',
+    'add_ptr_lbl_ptr_value' => 'ערך:',
+    'add_ptr_lbl_ptr_ttl' => 'PTR TTL:',
+    'tbl_head_zone_name' => 'שם אזור',
+    'tbl_head_zone_ip' => 'IP',
+    'tbl_head_zone_value' => 'ערך',
+    'tbl_lbl_edit' => 'עריכה',
+    'tbl_lbl_delete' => 'מחיקה',
+    'confirm_remove_ptr' => 'האם אתה בטוח שאתה רוצה להסיר אזור ורשומת PTR אלה',
+    'info_no_any_active' => 'עדיין לא קיימות רשומות.',
+    'info_not_enabled' => 'ניהול DNS הופכי לא אופשר',
+    'info_not_supported' => 'ניהול DNS הופכי אינו נתמך על ידי המודול',
+    'edit_ptr_lbl_ptr_name' => 'שם:',
+    'edit_ptr_lbl_ptr_value' => 'ערך:',
+    'edit_ptr_lbl_ptr_ttl' => 'TTL:',
+    'edit_ptr_btn_save' => 'שמור',
+    'edit_ptr_info_success' => 'הצלחה',
+    'edit_ptr_info_error' => 'שגיאה',
+    'edit_ptr_info_ajax_error' => 'שגיאת Ajax',
+    'zone_exists_error_part1' => 'אזור',
+    'zone_exists_error_part2' => 'או שרשומת PTR כבר קיימת',
+    'zone_format_error' => 'פורמט אזור שגוי',
+    'zone_create_info' => 'האזור נוצר בהצלחה',
+    'record_create_info' => 'הרשומה נוצרה',
+    'record_remove_info' => 'הרשומה הוסרה',
+    'zone_terminate_info' => 'אזור ורשומת DNS הפוכה כובו',
+    'error1' => 'אזור או רשומת PTR לא קיימים'
+    
+    
+);    
+
+?>
