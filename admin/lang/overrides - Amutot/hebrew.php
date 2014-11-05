c952e53db6f9d407d32444213a394eced84780db
Shalom Burla <shalom@starltd.net>
2014-11-05 11:25:28 +0200

    update langs for Amutot

---
+++ admin/lang/overrides - Amutot/hebrew.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WHMCS Language File
+ * Hebrew (he)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   http://docs.whmcs.com/Language_Overrides
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2013
+ * @license    http://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       http://www.whmcs.com/
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+$_ADMINLANG['invoices']['viewinvoices'] = "צפה בחשבוניות עסקה ";
+$_ADMINLANG['clientsummary']['invoices'] = "חשבוניות עסקה";
+$_ADMINLANG['orders']['geninvoice'] = "צור חשבונית עסקה ";
+$_ADMINLANG['invoices']['create'] = "צור חשבונית עסקה";
---
+++ lang/overrides - Amutot/hebrew.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * WHMCS Language File
+ * Hebrew (he)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   http://docs.whmcs.com/Language_Overrides
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2013
+ * @license    http://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       http://www.whmcs.com/
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+$_LANG['invoiceaddcreditdesc2'] = "זה יכול להיות מיושם על החשבונית עסקה באמצעות הטופס הבא.";
+$_LANG['invoicesinvoicedto'] = "חשבונית עסקה אל";
+$_LANG['invoicesinvoicenotes'] = "הערות לחשבונית עסקה";
+$_LANG['invoicesnoinvoices'] = "אין חשבוניות עסקה";
+$_LANG['invoicesoutstandinginvoices'] = "יתרת החשבונית עסקה";
+$_LANG['invoicesview'] = "צפה בחשבונית עסקה";
+$_LANG['ordercompletebutnotpaid'] = "שם לב! תהליך ההזמנה הסתיים אך עדיין לא התקבל התשלום עבור ההזמנה, ההזמנת תופעל ברגע שהתשלום יתקבל.<br />לחץ על הקישור הבא כדי לראות את החשבונית עסקה ולבצע תשלום.";
+$_LANG['paidinvoicename'] = "קבלה #";
+$_LANG['invoicespaidinvto'] = "קבלה אל";
